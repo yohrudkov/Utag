d781c39a0d3d720b72957a70a020fe1a16b52a6e
Yaroslav Ohrudkov <yohrudkov@e1r7p12.unit.ua>
2020-12-19 12:54:55 +0200

    Utag

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-export PKG_CONFIG_PATH="-L~/.linuxbrew/Cellar/qt/5.15.2/lib/pkgconfig"
-export CMAKE_PREFIX_PATH="~/.linuxbrew/Cellar/qt/5.15.2/lib/cmake:$CMAKE_PREFIX_PATH"
+# export PKG_CONFIG_PATH="-L~/.linuxbrew/Cellar/qt/5.15.2/lib/pkgconfig"
+# export CMAKE_PREFIX_PATH="~/.linuxbrew/Cellar/qt/5.15.2/lib/cmake:$CMAKE_PREFIX_PATH"
 
 cmake . -Bbuild -Wdev -Werror=dev && cmake --build ./build
 cp ./build/app/utag ./utag
